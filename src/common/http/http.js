9de0348d0148cdffad26458b0273b38baa1f5d37
liaozhenxing <liaozx1802@163.com>
2021-10-21 17:48:08 +0800

    feat: 重新整理http请求服务

---
+++ src/common/http/http.js
@@ -0,0 +1,117 @@
+
+/**
+ * 基本的http请求封装
+ * 注意：
+ * - 业务层的http请求请直接使用service层的
+ * - 尽量不要在这里添加业务相关代码
+ * 功能：
+ * - 请求promise化
+ * - 支持重试（2次，可修改）
+ * -支持abort中断请求
+ */
+import { LOGIN_API, RECORD_MSG } from '@/common/utils/constant'
+import { baseApi } from '@/config'
+
+const MAX_RETRY_TIME = 2;                                         // 重试次数
+const RETRY_TIME_LIST = [50, 100, 200];                           // 重试的延迟时间
+const RETRY_BLACK_LIST = [LOGIN_API, RECORD_MSG];   // 无需重试的接口
+
+if (baseApi.endsWith('/')) {
+    baseApi = baseApi.substr(0, baseApi.length-1);
+  }
+
+export class HttpRequest {
+    retryTime = 0
+    task = undefined
+    /**
+     * get请求
+     * @param {string} url 
+     * @param {object} data 
+     * @param {string} token 
+     * @param {object} header 
+     */
+    get(url, data = {}, token, header) {
+        return this.request(url, data, 'GET', token, header)
+    }
+
+    /**
+     * post请求
+     * @param {string} url 
+     * @param {object} data 
+     * @param {string} token 
+     * @param {object} header 
+     */
+    post(url, data = {}, token, header) {
+        return this.request(url, data, 'POST', token, header)
+    }
+
+    /**
+     * 一般请求方法
+     * @param {string} url 
+     * @param {object} data 
+     * @param {string} method 
+     * @param {string} token 
+     * @param {object} header 
+     */
+    request(url, data = {}, method, token, header) {
+        return new Promise((resolve, reject) => {
+            let success = (res) => {
+                resolve(res)
+            }
+            let fail = (error) => {
+                reject(error)
+            }
+            header = header || {
+                'Content-Type': 'application/json',
+                'cache-control': 'no-cache',
+                'Authorization': token
+              }
+            this.requestInternal(url, header, method, data, success, fail)
+        })
+    }
+    abort() {
+        this.task && this.task.abort()
+    }
+    requestInternal(url, header, method, data, success, fail, responseType = 'text') {
+        let urlPath = url;
+        let httpPatter = /^http/i;
+        if (!httpPatter.test(url)) {
+            url = url.startsWith('/') ? url : "/"+url;
+            urlPath = baseApi + url;
+        }
+        this.task = wx.request({
+            url: urlPath,
+            header,
+            method,
+            data,
+            responseType,
+            success: (resp) => {
+                if ((resp.statusCode !== 200 || !resp.data) && this.retryTime < MAX_RETRY_TIME) {
+                    if (RETRY_BLACK_LIST.includes(url)) {
+                        fail(new Error(resp.data ? JSON.stringify(resp.data) : "没有返回数据", resp.statusCode))
+                      } else {
+                        setTimeout(() => {
+                          this.requestInternal(url, header, method, data, success, fail, responseType);
+                        }, RETRY_TIME_LIST[this.retryTime]);
+                        this.retryTime += 1;
+                      }
+                } else if (resp.statusCode === 200) {
+                    success(resp.data)
+                } else {
+                    fail(new Error(JSON.stringify(resp.data), resp.statusCode))
+                }
+            },
+            fail: (err) => {
+                // 微信请求失败
+                if (err.errMsg.indexOf('request:fail') !== -1 && this.retryTime < MAX_RETRY_TIME) {
+                    setTimeout(() => {
+                      this.requestInternal(url, header, method, data, success, fail, responseType);
+                    }, RETRY_TIME_LIST[this.retryTime]);
+                    this.retryTime += 1;
+                  } else {
+                    fail(new Error(err.errMsg, -1));
+                  }
+            }
+        })
+    }
+}
---
+++ src/services/http.js
@@ -0,0 +1,98 @@
+import { LOGIN_API, RECORD_MSG } from '@/common/utils/constant'
+import { getToken } from '@/common/utils/auth'
+import { HttpRequest } from '@/common/http/http'
+
+// 不需要token的白名单
+const WhiteList = [LOGIN_API, RECORD_MSG]
+
+// 最大同时请求数
+const MAX_REQUEST = 10
+let isLogin = true
+
+export class HttpService {
+  // 等待请求队列
+  waitQueue = []
+  // 当前请求数
+  runningCount = 0
+  /**
+   * get 请求
+   * @param {string} url 
+   * @param {object} data 
+   */
+  get(url, data = {}) {
+    return new Promise((resolve, reject) => {
+      this.requstInernal('GET', url, data, (resp) => {
+        resolve(resp)
+      }, (err) => {
+        reject(err)
+      })
+    })
+  }
+  /**
+   * post 请求
+   * @param {string} url 
+   * @param {object} data 
+   */
+  post(url, data = {}) {
+    return new Promise((resolve, reject) => {
+      this.requstInernal('POST', url, data, (resp) => {
+        resolve(resp)
+      }, (err) => {
+        reject(err)
+      })
+    })
+  }
+  /**
+   * 执行队列里面的请求
+   */
+  next() {
+    // 是否已登录(根据业务需要)
+    if (isLogin) {
+      while (this.waitQueue.length && this.runningCount < MAX_REQUEST) {
+        const item = this.waitQueue.shift()
+        this.requstInernal(item.method, item.path, item.data, item.success, item.fail)
+      }
+    }
+  }
+
+  requstInernal(method, path, data, success, fail) {
+    let token = getToken() || ''
+    if (WhiteList.indexOf(path) > -1) {
+      token = ''
+    } else if (!token) {
+      // 既不是免登录接口，也没有token，把接口信息存储到队列中
+      this.waitQueue.push({ path, method, data, success, fail });
+      return;
+    }
+    if (this.runningCount > MAX_REQUEST) {
+      this.waitQueue.push({ path, method, data, success, fail });
+      return;
+    }
+    this.runningCount++
+    let promise = new HttpRequest().request(path, data, method, token)
+    promise.then((resp) => {
+      if (resp.code === '200') {
+        success && success(resp.data);
+      } else if (resp.code === 101) {
+        // token过期处理
+        if (path !== LOGIN_API) {
+          this.waitQueue.push({path, method, data, success, fail});
+        }
+        // token 过期, 需要重新获取token
+        // this.$service.login.reLogin();
+      } else if(resp.code === 104) {
+        let err = new Error("版本过低", resp.code)
+        fail && fail(err)
+      } else {
+        let err = new Error(resp.message, resp.code)
+        fail && fail(err)
+      }
+    }).catch((err) => {
+      fail && fail(new Error('', err.code));
+    }).finally(() => {
+      this.runningCount = Math.max(0, --this.runningCount);
+      this.next();
+    })
+
+  }
+}
